75ee110457597cc7f43ac288379ed09abbd6801b
ducnmm <ducnmm@pascaliaasia.com>
2025-07-07 12:53:13 +0700

    test: trigger app-main dependency build Mon Jul  7 12:53:12 +07 2025

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Test app-main build
+// Test app-main dependency build - Mon Jul  7 12:53:12 +07 2025

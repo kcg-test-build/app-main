2d41870170c8b1f8f7c3369f0aa2a2f2240f0bd6
ducnmm <ducnmm@pascaliaasia.com>
2025-07-07 15:19:14 +0700

    Second test commit for build pipeline
    - Added timestamp output with DateTime.Now
    - Added comment with current time
    - Testing pipeline monitoring Round 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Testing build pipeline trigger!");
+Console.WriteLine($"Build triggered at: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
 // Test app-main build
 // Test app-main dependency build - Mon Jul  7 12:53:12 +07 2025
 // New commit to test build pipeline - $(date)
+// Second commit test - July 7, 2025 3:17 PM

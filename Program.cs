b88419b7537b2a44a3baaf2c0b1dc8e8d0a25dce
ducnmm <ducnmm@pascaliaasia.com>
2025-07-07 14:57:31 +0700

    Add console output to test build pipeline trigger
    - Added new console WriteLine to test pipeline
    - Added timestamp comment for tracking

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Testing build pipeline trigger!");
 // Test app-main build
 // Test app-main dependency build - Mon Jul  7 12:53:12 +07 2025
+// New commit to test build pipeline - $(date)
